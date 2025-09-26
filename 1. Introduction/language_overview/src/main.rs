6536bcf8f6a23d597c0125346fb03b13ff57d98e
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-09-25 19:52:29 -0600

    Refactor main function to include sign function calls and their outputs

--- 1. Introduction/language_overview/src/main.rs
+++ 1. Introduction/language_overview/src/main.rs
@@ -2,6 +2,21 @@ fn main() {
     hello();
     let x = 34;
     println!("{}! = {}", x, fact(x));
+
+    let s1: i32 = sign(5);
+    let s2: i32 = sign(-10);
+    let s3: i32 = sign(0);
+
+    println!("s1 = {}", s1);
+    println!("s2 = {}", s2);
+    println!("s3 = {}", s3);
+
+    let n = -100;
+    match sign(n) {
+        -1 => println!("{} es negativo", n),
+        1  => println!("{} es positivo", n),
+        _  => println!("{} es cero", n) 
+    }
 }
 
 fn hello() {
@@ -10,8 +25,35 @@ fn hello() {
 
 fn fact(n: u8) -> u128 {
     let mut result: u128 = 1;
-    for i in 1..=n {
+
+    // for i in 1..=n {
+    //     result *= i as u128;
+    // }
+
+    let mut i: u8 = 1;
+
+    // while i <= n {
+    //     result *= i as u128;
+    //     i += 1;
+    // }
+
+    loop {
+        if i > n {
+            break;
+        }
         result *= i as u128;
+        i += 1;
     }
+
     result
+}
+
+fn sign(n: i32) -> i32 {
+    if n < 0 {
+        -1
+    } else if n > 0 {
+        1
+    } else {
+        0
+    }
 }
