feb7591c2611a7eb76348dea95dd14cae4729227
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-09-29 18:39:07 -0600

    Add Color enum and update main function to demonstrate color usage

--- 2. Enums and Structs/enum_examples/src/main.rs
+++ 2. Enums and Structs/enum_examples/src/main.rs
@@ -3,6 +3,16 @@ enum CardinalPoints {
     North, South, East, West
 }
 
+#[derive(Debug)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+    Black,
+    White,
+    RGB(u8, u8, u8)
+}
+
 fn main() {
     let point1 = CardinalPoints::North;
     let mut point2: CardinalPoints = CardinalPoints::East;
@@ -21,5 +31,21 @@ fn main() {
         CardinalPoints::North => println!("Norte"),
         CardinalPoints::South => println!("Sur"),
         _ => println!("Otra")
+    }
+
+    let color1 = Color::Red;
+    let color2 = Color::White;
+    let color3 = Color::RGB(0, 255, 255);
+    let colores = vec![Color::Green, Color::Blue, Color::Black];
+
+    println!("{:?} {:?} {:?} {:?}", color1, color2, color3, colores);
+
+    match color3 {
+        Color::Red => println!("Soy rojo"),
+        Color::Green => println!("Soy verde"),
+        Color::Blue => println!("Soy azul"),
+        Color::Black => println!("Soy negro"),
+        Color::White => println!("Soy blanco"),
+        Color::RGB(r, g, b) => println!("R={}, G={}, B={}", r, g, b)
     }
 }
