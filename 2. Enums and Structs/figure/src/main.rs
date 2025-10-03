b6cf58f7455c1691d3947fc29675694002acfaca
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-10-02 18:50:23 -0600

    Add Triangle struct and implement Figure trait; refactor main function to use report

--- 2. Enums and Structs/figure/src/main.rs
+++ 2. Enums and Structs/figure/src/main.rs
@@ -31,7 +31,7 @@ impl Figure for Square {
     }
 
     fn description(&self) -> String {
-        "Un cuadrado".to_string()
+        format!("Square (side = {} m)", self.side)
     }
 }
 
@@ -57,16 +57,67 @@ impl Figure for Circle {
     }
 }
 
+#[derive(Debug)]
+struct Triangle {
+    base: f64,
+    height: f64
+}
+
+
+impl Triangle {
+    fn new(base: f64, height: f64) -> Self {
+        Self { base, height }
+    }
+}
+
+impl Figure for Triangle {
+
+    fn area(&self) -> f64 {
+        self.base * self.height / 2.0
+    }
+
+    fn perimeter(&self) -> f64 {
+        self.base * 3.0
+    }
+
+    fn description(&self) -> String {
+        format!("Triangle (base = {} m, height = {} m)", self.base, self.height)
+    }
+}
+
+fn report<T: Figure>(figure: &T) {
+    println!("FIGURE: {}", figure.description());
+    println!("------------------------------------");
+    println!("Área = {:.2} m^2", figure.area());
+    println!("Perímetro = {:.2} m", figure.perimeter());
+    println!();
+}
+
+impl Figure for i32 {
+    
+    fn area(&self) -> f64 {
+        0.0
+    }
+
+    fn perimeter(&self) -> f64 {
+        0.0
+    }
+
+    fn description(&self) -> String {
+        format!("i32 = {}", self)
+    }
+}
+
 fn main() {
     let s = Square::new(3.0);
-    println!("{:?}", s);
-    println!("Área = {}", s.area());
-    println!("Perímetro = {}", s.perimeter());
-    println!("Descripción = {}", s.description());
+    report(&s);
 
     let c = Circle::new(10.0);
-    println!("{:?}", c);
-    println!("Área = {}", c.area());
-    println!("Perímetro = {}", c.perimeter());
-    println!("Descripción = {}", c.description());
+    report(&c);
+
+    let t = Triangle::new(4.5, 2.0);
+    report(&t);
+
+    let x: i32 = 7;
+    report(&x);
 }
