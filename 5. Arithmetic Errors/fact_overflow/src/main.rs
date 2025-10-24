7213e5aa251bc299e03d36d9140a87b32d08d465
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-10-23 19:22:08 -0600

    Refactor factorial function to return Option<u128> and update main for better error handling

--- 5. Arithmetic Errors/fact_overflow/src/main.rs
+++ 5. Arithmetic Errors/fact_overflow/src/main.rs
@@ -1,13 +1,17 @@
 fn main() {
-    let x = 35;
-    let r = fact(x);
-    println!("{}! = {}", x, r);
+    let x: u8 = 35;
+    let r: Option<u128> = fact(x);
+    println!("{}! = {:?}", x, r.unwrap_or(0));
+    println!("{}! = {:?}", x, fact(34).unwrap_or(0));
 }
 
-fn fact(n: u8) -> u128 {
+fn fact(n: u8) -> Option<u128> {
     let mut result: u128 = 1;
     for i in 2..=n {
-        result *= i as u128;
+        result = match result.checked_mul(i as u128) {
+            Some(x) => x,
+            None => return None
+        }
     }
-    result
+    Some(result)
 }
