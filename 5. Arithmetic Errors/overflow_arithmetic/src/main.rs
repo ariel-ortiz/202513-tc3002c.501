5e87a914e862674783225c5bb37c80f5daa92381
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-10-23 19:06:25 -0600

    Add implementations for overflow, saturating, and wrapping arithmetic functions

--- 5. Arithmetic Errors/overflow_arithmetic/src/main.rs
+++ 5. Arithmetic Errors/overflow_arithmetic/src/main.rs
@@ -1,5 +1,10 @@
+use std::u8;
+
 fn main() {
     demo_checked();
+    demo_overflowing();
+    demo_saturating();
+    demo_wrapping();
 }
 
 fn demo_checked() {
@@ -10,4 +15,53 @@ fn demo_checked() {
         Some(valor) => println!("{}", valor),
         None => println!("No hay resultado")
     }
+    let x: u8 = 1;
+    let y: u8 = 250;
+    let z: u8 = x.checked_add(y).unwrap_or(u8::MAX);
+    println!("{}", z);
+    let x: u8 = 10;
+    let y: u8 = 250;
+    let z: u8 = x.checked_add(y).unwrap_or(u8::MAX);
+    println!("{}", z);
+}
+
+fn demo_overflowing() {
+    let x: u8 = 10;
+    let y: u8 = 250;
+    let z: (u8, bool) = x.overflowing_add(y);
+    println!("{:?}", z);
+    let x: u8 = 1;
+    let y: u8 = 250;
+    let z: (u8, bool) = x.overflowing_add(y);
+    println!("{:?}", z);
+}
+
+fn demo_saturating() {
+    let x: u8 = 10;
+    let y: u8 = 250;
+    let z: u8 = x.saturating_add(y);
+    println!("{}", z);
+
+    let x: u8 = 10;
+    let y: u8 = 20;
+    let z: u8 = x.saturating_sub(y);
+    println!("{}", z);
+
+    let x: u8 = 10;
+    let y: u8 = 7;
+    let z: u8 = x.saturating_sub(y);
+    println!("{}", z);
+}
+
+fn demo_wrapping() {
+    let x: u8 = 10;
+    let y: u8 = 250;
+    let z: u8 = x.wrapping_add(y);
+    println!("{}", z);
+
+    let x: u8 = 1;
+    let y: u8 = 250;
+    let z: u8 = x.wrapping_add(y);
+    println!("{}", z);
+
 }
